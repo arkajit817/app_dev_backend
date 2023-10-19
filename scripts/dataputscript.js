2a764471003221ebb561237aecfce5b3d70fb219
Arkajit Choudhury <arkajitchoudhury@Arkajits-MacBook-Pro.local>
2023-10-19 17:59:52 +0530

    json upload

--- scripts/dataputscript.js
+++ scripts/dataputscript.js
@@ -3,27 +3,20 @@
 const config = require("./../src/server/config/index");
 const MongoClient = require('mongodb').MongoClient;
 const treatmentSchema = require("./../src/server/models/treatment");
-let obj = [{
-  userId: '64f73fcaaa0e0a82ab482d9a',
-  patient: 'arkajit',
-  treatment_start_date: '2023-12-01',
-  treatment_end_date: '2023-12-01',
-  symptoms: ['aids', 'std'],
-  treatment_goal: ['protected sex'],
-  associated_tests: ['abc'],
-  medication: ['abc'],
-  medical_procedures: ['abc'],
-  medical_advice: ['abc'],
-  patient_requests: ['qwer'],
-  diagnosis: ['condom'],
-  institutions: ['asasa'],
-  status: 'true',
-  comments: ["asdfgh"],
-  treating_doctor: ['6512f37fa75bb1e6803abda6']
-}];
+
+const fs = require('fs')
+const path = 'sample.json'
+
+let filedata = fs.readFileSync(path,'utf8');
+console.log(filedata,"file");
+
+const data = JSON.parse(filedata)
+
+
+
 require('mongoose').connect(`${config.dbUrl}`).then(async(err,db) => {
   console.log("connected to mongoDB");
-  for (let o of obj){
+  for (let o of data){
     await treatmentSchema.create(o).then((output) => {
       console.log(output);
       console.log(db);
